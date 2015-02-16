1338d4644e5e6cd01d5886be577c4b9a36a3c970
Nishanth <Nishanth@172.31.138.92>
2015-02-16 12:44:32 -0500

    demo project

--- Demo/src/demo/Demo.java
+++ Demo/src/demo/Demo.java
@@ -1,5 +1,14 @@
 package demo;
 
 public class Demo {
-	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+		
+		for(int i = 0;i < 5;i++)
+		{
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
 }
